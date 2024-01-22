72a9b6a2ea85f5ec32c22c0a1a8325d589997f70
Iram Farooqui <iram.farooqui2000@gmail.com>
2024-01-22 17:43:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function openSinglePlayerGame(){
+    window.location.href="singlePlayer.html";
+}
+function openMultiPlayerGame(){
+    window.location.href="multiplayer.html";
+}
+function mainMenu(){
+    history.go(-1);
+}
+function openSnakeGame(){
+    window.location.href="snakeGame.html";
+}
+
+function ticTacToe(){
+    window.location.href="tictactoe.html";
+}
+function openPongGame(){
+    window.location.href="pong.html";
+}
+function openCandyCrush(){
+    window.location.href="candy.html"
+}
